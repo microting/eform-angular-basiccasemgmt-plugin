2c7393f6c8c93bcf42634a2bdb7e2d552269f1b6
Leonid <em-atf@mail.ru>
2018-09-25 14:47:41 +0300

    Added base API project

---
+++ eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn/Infrastructure/Data/CaseManagementPnDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManagement.Pn.Infrastructure.Data
+{
+    public class CaseManagementPnDbContext : DbContext
+    {
+        public CaseManagementPnDbContext()
+            : base("eFormCaseManagementPnConnection")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer<CaseManagementPnDbContext>(null);
+        }
+
+        public CaseManagementPnDbContext(string connectionString)
+            : base(connectionString)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer<CaseManagementPnDbContext>(null);
+        }
+
+        public static CaseManagementPnDbContext Create()
+        {
+            return new CaseManagementPnDbContext();
+        }
+
+     //   public DbSet<Customer> Customers { get; set; }
+    //    public DbSet<Field> Fields { get; set; }
+     //   public DbSet<CustomerField> CustomerFields { get; set; }
+    //    public DbSet<CustomerSettings> CustomerSettings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Customer>()
+            //    .Property(e => e.RelatedEntityId)
+            //    .HasColumnAnnotation(
+            //        IndexAnnotation.AnnotationName,
+            //        new IndexAnnotation(new IndexAttribute { IsUnique = true }));
+        }
+    }
+}
