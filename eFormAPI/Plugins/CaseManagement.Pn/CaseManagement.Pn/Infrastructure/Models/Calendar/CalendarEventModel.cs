ad83201b1606216742f65635afd642590003d3b5
Leonid <em-atf@mail.ru>
2018-09-26 14:35:20 +0300

    Added get calendar events

---
+++ eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn/Infrastructure/Models/Calendar/CalendarEventModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaseManagement.Pn.Infrastructure.Models.Calendar
+{
+    public class CalendarEventModel
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Title { get; set; }
+        public string Color { get; set; }
+        public List<CalendarEventMeta> Meta { get; set; } 
+            = new List<CalendarEventMeta>();
+
+    }
+}
