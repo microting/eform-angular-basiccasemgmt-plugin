25bece4c5792e30db85e93fbf51615b44f32d434
René Schultz Madsen <rm@microting.com>
2018-10-30 13:40:17 +0100

    Fixing the broken comparsion of values.

--- eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn/Controllers/CalendarController.cs
+++ eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn/Controllers/CalendarController.cs
@@ -39,7 +39,7 @@ namespace CaseManagement.Pn.Controllers
                 List<int> casesSiteIds = cases.Where(x => x.SiteId != null)
                     .Select(x => x.SiteId)
                     .GroupBy(x => x)
-                    .Select(x => (int) x.Key)
+                    .Select(x => (int)x.Key)
                     .ToList();
 
 
@@ -52,17 +52,17 @@ namespace CaseManagement.Pn.Controllers
                     if (caseItem.SiteId != null)
                     {
                         int? siteId = caseItem.SiteId;
-                        int relatedEntityId = int.Parse(caseItem.FieldValue3); 
+
                         // We change this, because the reporting user is not the same as the one that is going to finish the task.
                         // This should be a dynamic change based on settings in a later version.
                         // CalendarUser calendarUser = calendarUsers.FirstOrDefault(x => x.SiteId == siteId);
-                        CalendarUser calendarUser = calendarUsers.FirstOrDefault(x => x.RelatedEntityId == relatedEntityId);
+                        CalendarUser calendarUser = calendarUsers.FirstOrDefault(x => x.NameInCalendar == caseItem.FieldValue3);
                         if (calendarUser != null)
                         {
-                            var dateParseResult = DateTime.TryParseExact(caseItem.FieldValue1, 
-                                "yyyy-MM-dd", 
+                            var dateParseResult = DateTime.TryParseExact(caseItem.FieldValue1,
+                                "yyyy-MM-dd",
                                 null,
-                                DateTimeStyles.None, 
+                                DateTimeStyles.None,
                                 out DateTime date);
                             var item = new CalendarEventModel
                             {
