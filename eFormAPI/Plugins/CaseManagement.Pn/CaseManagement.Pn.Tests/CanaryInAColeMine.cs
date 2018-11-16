e1b48ea21be302a5e2eed1afec20696aa51d8126
René Schultz Madsen <rm@microting.com>
2018-11-16 13:46:42 +0100

    Removing unwanted migrations.

---
+++ eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn.Tests/CanaryInAColeMine.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace CaseManagement.Pn.Tests
+{
+    [TestFixture]
+    public class CanaryInAColeMine
+    {
+        [Test]
+        public void CanPeepUnit()
+        {
+            Assert.True(true);
+        }
+    }
+}
