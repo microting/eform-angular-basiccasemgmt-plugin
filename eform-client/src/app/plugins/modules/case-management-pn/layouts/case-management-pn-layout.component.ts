b576dc82e0270e783371d518bb872890c993aaf8
Gid <gid733@ya.ru>
2018-12-04 13:35:00 +0200

    Added pagesize & sorting save

--- eform-client/src/app/plugins/modules/case-management-pn/layouts/case-management-pn-layout.component.ts
+++ eform-client/src/app/plugins/modules/case-management-pn/layouts/case-management-pn-layout.component.ts
@@ -1,15 +1,21 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {LocaleService} from 'src/app/common/services/auth';
+import {CaseManagementPnLocalSettings} from '../enums';
+import {SharedPnService} from '../../shared/services';
 declare var require: any;
 
 @Component({
   selector: 'app-customers-pn-layout',
   template: `<router-outlet></router-outlet>`
 })
-export class CaseManagementPnLayoutComponent implements AfterViewInit {
-  constructor(private localeService: LocaleService, private translateService: TranslateService) {
+export class CaseManagementPnLayoutComponent implements AfterViewInit, OnInit {
+  constructor(private localeService: LocaleService,
+              private translateService: TranslateService,
+              private sharedPnService: SharedPnService) {}
 
+  ngOnInit() {
+    this.sharedPnService.initLocalPageSettings('caseManagementPnSettings', CaseManagementPnLocalSettings);
   }
 
   ngAfterViewInit() {
