f72ca3d6152453eb0b1bb70ea949cfdd00f2bb12
René Schultz Madsen <rm@microting.dk>
2019-11-05 06:57:36 +0100

    Adding changes to have the permission versions.

--- eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn/Migrations/20191105055626_AddingPluginPermissionVersions.Designer.cs
+++ eFormAPI/Plugins/CaseManagement.Pn/CaseManagement.Pn/Migrations/20191105055626_AddingPluginPermissionVersions.Designer.cs
@@ -4,14 +4,16 @@ using CaseManagement.Pn.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CaseManagement.Pn.Migrations
 {
     [DbContext(typeof(CaseManagementPnDbContext))]
-    partial class CaseManagementPnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191105055626_AddingPluginPermissionVersions")]
+    partial class AddingPluginPermissionVersions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             string autoIDGenStrategy = "SqlServer:ValueGenerationStrategy";
