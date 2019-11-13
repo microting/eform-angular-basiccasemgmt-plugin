cd35a7100077928b09c829e73e39ba153e124e32
René Schultz Madsen <rm@microting.dk>
2019-11-13 08:16:56 +0100

    Fixing the broken e2e tests.

--- eform-client/e2e/Tests/case-management-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/case-management-settings/application-settings.plugins-page.spec.ts
@@ -1,5 +1,6 @@
 import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
+import pluginPage from '../../Page objects/Plugin.page';
 
 import {expect} from 'chai';
 import pluginsPage from './application-settings.plugins.page';
@@ -12,7 +13,8 @@ describe('Application settings page - site header section', function () {
         loginPage.login();
         myEformsPage.Navbar.advancedDropdown();
         myEformsPage.Navbar.clickonSubMenuItem('Plugins');
-        browser.pause(40000);
+        browser.waitForExist('#plugin-name', 50000);
+        browser.pause(10000);
 
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
@@ -29,18 +31,12 @@ describe('Application settings page - site header section', function () {
         browser.pause(50000); // We need to wait 50 seconds for the plugin to create db etc.
         browser.refresh();
 
-        // Start - This block is here because of the new plugin permission loading, requires a re-login.
-        browser.waitForVisible('#plugin-id', 40000);
-        browser.pause(10000);
-        myEformsPage.Navbar.logout();
         loginPage.login();
         myEformsPage.Navbar.advancedDropdown();
         myEformsPage.Navbar.clickonSubMenuItem('Plugins');
         browser.waitForExist('#plugin-name', 50000);
         browser.pause(10000);
-        // End - This block is here because of the new plugin permission loading, requires a re-login.
 
-        browser.waitForVisible('#plugin-id', 40000);
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting Case Management plugin');
