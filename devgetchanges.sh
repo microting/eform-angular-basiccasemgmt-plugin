0b7d6100d63683e1b03be29043d83f9dbf5801e1
René Schultz Madsen <rm@microting.com>
2020-04-01 10:08:30 +0200

    Fixing the devgetchanges.sh

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -2,24 +2,24 @@
 
 cd ~
 
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/case-management-pn
+rm -fR Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/src/app/plugins/modules/case-management-pn
 
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/case-management-pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/case-management-pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/case-management-pn Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/src/app/plugins/modules/case-management-pn
 
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/CaseManagement.Pn
+rm -fR Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eFormAPI/Plugins/CaseManagement.Pn
 
-cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/CaseManagement.Pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/CaseManagement.Pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/CaseManagement.Pn Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eFormAPI/Plugins/CaseManagement.Pn
 
 # Test files rm
 
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/case-managements-settings
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/case-management-general
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/case-management
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+rm -fR Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/e2e/Tests/case-managements-settings
+rm -fR Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/e2e/Tests/case-management-general
+rm -fR Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/e2e/Page\ objects/case-management
+rm -fR Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/wdio-headless-plugin-step2.conf.js
 
 # Test files cp
 
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/case-managements-settings Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/case-managements-settings
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/case-management-general Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/case-management-general
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/case-management Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/case-management
-cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/case-managements-settings Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/e2e/Tests/case-managements-settings
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/case-management-general Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/e2e/Tests/case-management-general
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/case-management Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/e2e/Page\ objects/case-management
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-basiccasemgmt-plugin/eform-client/wdio-headless-plugin-step2.conf.js
