4b3696af7fa7ddf6222a71874a8cce331f3f0a1f
René Schultz Madsen <rm@microting.dk>
2020-04-01 06:51:21 +0200

    Adding the missing ,

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-perl -pi -e '$_.="  }\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  loadChildren: '\''./modules/case-management-pn/case-management-pn.module#CaseManagementPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  path: '\''case-management-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  {\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
-
