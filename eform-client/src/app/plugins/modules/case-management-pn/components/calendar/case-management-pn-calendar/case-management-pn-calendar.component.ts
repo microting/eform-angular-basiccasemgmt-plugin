1e5f5a22f0cb83d2ecb5da08abd99fe01fd559d5
René Schultz Madsen <rm@microting.dk>
2020-04-17 12:50:41 +0200

    Commenting the broken code for angular 9.

--- eform-client/src/app/plugins/modules/case-management-pn/components/calendar/case-management-pn-calendar/case-management-pn-calendar.component.ts
+++ eform-client/src/app/plugins/modules/case-management-pn/components/calendar/case-management-pn-calendar/case-management-pn-calendar.component.ts
@@ -58,7 +58,7 @@ export class CaseManagementPnCalendarComponent implements OnInit {
   }
 
   ngOnInit() {
-    let userLocale = this.localeService.getCurrentUserLocale();
+    const userLocale = this.localeService.getCurrentUserLocale();
     if (userLocale === 'da-DK') {
       this.locale = 'da';
     } else {
@@ -102,7 +102,7 @@ export class CaseManagementPnCalendarComponent implements OnInit {
     this.calendarService.getCalendarEvents(this.calendarEventsRequestModel).subscribe((data) => {
       if (data && data.success) {
         if (data.model.length > 0) {
-          for (let calendarEvent of data.model) {
+          for (const calendarEvent of data.model) {
             this.events.push(
               {
                 start: toDate(calendarEvent.start),
--- eform-client/src/app/plugins/modules/case-management-pn/services/calendar/custom-event-title-formatter.provider.ts
+++ eform-client/src/app/plugins/modules/case-management-pn/services/calendar/custom-event-title-formatter.provider.ts
@@ -3,9 +3,9 @@ import { CalendarEventTitleFormatter, CalendarEvent } from 'angular-calendar';
 import { DatePipe } from '@angular/common';
 
 export class CustomEventTitleFormatter extends CalendarEventTitleFormatter {
-  constructor(@Inject(LOCALE_ID) private locale: string) {
-    super();
-  }
+  // constructor(@Inject(LOCALE_ID) private locale: string) {
+  //   super();
+  // }
 
   // you can override any of the methods defined in the parent class
 
